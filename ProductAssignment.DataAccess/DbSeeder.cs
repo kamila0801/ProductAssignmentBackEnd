2dfa1a17dd7a90ce3105a8bf68c4dbb203bf76f7
kamila0801 <kami7686@easv365.dk>
2021-11-17 23:37:01 +0100

    even more legoooo

--- ProductAssignment.DataAccess/DbSeeder.cs
+++ ProductAssignment.DataAccess/DbSeeder.cs
@@ -64,6 +64,54 @@ namespace ProductAssignment.DataAccess
                 Color = "blue",
                 Price = 23.00
             });
+            _ctx.Products.Add(new ProductEntity
+            {
+                Name = "blue lego",
+                Color = "blue",
+                Price = 12.25
+            });
+            _ctx.Products.Add(new ProductEntity
+            {
+                Name = "red lego",
+                Color = "red",
+                Price = 11.25
+            });
+            _ctx.Products.Add(new ProductEntity
+            {
+                Name = "purple lego",
+                Color = "yellow",
+                Price = 13.00
+            });
+            _ctx.Products.Add(new ProductEntity
+            {
+                Name = "happy lego",
+                Color = "yellow",
+                Price = 13.00
+            });
+            _ctx.Products.Add(new ProductEntity
+            {
+                Name = "sad lego",
+                Color = "blue",
+                Price = 7.50
+            });
+            _ctx.Products.Add(new ProductEntity
+            {
+                Name = "small lego",
+                Color = "black",
+                Price = 5.00
+            });
+            _ctx.Products.Add(new ProductEntity
+            {
+                Name = "big lego",
+                Color = "white",
+                Price = 13.00
+            });
+            _ctx.Products.Add(new ProductEntity
+            {
+                Name = "random lego",
+                Color = "blue",
+                Price = 23.00
+            });
             _ctx.SaveChanges();
         }
         
